0a64c6e767c77d2487f8b1f220ec4088995cc601
Alex Procton <aprocton@gmail.com>
2018-03-05 11:44:10 -0500

    modified seqlib.py to work with example code from PDSB

--- seqlib/seqlib.py
+++ seqlib/seqlib.py
@@ -1,69 +1,30 @@
+import copy
 import numpy as np
 import pandas as pd
-
-
-def mutate(base):
-    "Mutate a nucleobase to any different nucleobase"
-    diff = set("ACTG") - set(base)
-    return np.random.choice(list(diff))
-
-
-def filter_missing(arr, maxfreq):
-    """
-    Function to remove sites with a high frequency of missing reads from an array of DNA seqs
-
-    Args:
-        arr: The array of DNA sequences, with missing reads designated as N
-        maxfreq: The critical frequency for missing reads. If the frequency of N at a site
-                 exceeds maxfreq, that site is excluded from the result
-
-    Returns:
-        A view of arr including only columns in which the frequency of N <= maxfreq
-    """
-    # determine the frequency of missing reads at a site
-    freqmissing = np.sum(arr == "N", axis=0) / arr.shape[0]
-
-    # return arr, sliced to only include cols with freqmissing <= maxfreq
-    return arr[:, freqmissing <= maxfreq]
-
-
-def filter_maf(arr, minfreq):
-    """
-    Filters an array of seqs to only include sites at which a minimum
-    proportion of alleles differ from the majority
-
-    Args:
-        arr: the array of sequence data
-        minfreq: the minimum proportion of alleles which must differ from
-                 the most common allele at a site
-
-    Returns:
-        A view of the numpy array arr, filtered to include only columns in
-        which the minor allele frequency exceeds minfreq
-    """
-    # determine proportion of alleles in a col that do not match the first row
-    freqs = np.sum(arr != arr[0], axis=0) / arr.shape[0]
-
-    # create a copy of freqs with all values > 0.5 changed to their complement
-    maf = freqs.copy()
-    maf[maf > 0.5] = 1 - maf[maf > 0.5]
-
-    return arr[:, maf > minfreq]
 
 
 class Seqlib:
     def __init__(self, ninds, nsites):
         self.ninds = ninds
         self.nsites = nsites
-        self.seqs = self.simulate()
 
-    def simulate(self):
+        self.seqs = self._simulate()
+
+        self.maf = self._get_maf()
+
+    # private functions
+    def _mutate(self, base):
+        """Mutate a nucleobase to any different nucleobase"""
+        diff = set("ACTG") - set(base)
+        return np.random.choice(list(diff))
+
+    def _simulate(self):
         """
         Simulates ninds DNA sequences of length nsites
         Adds mutations and then missing reads (probability 0.1 for each)
 
         Returns:
-            A numpy array of dimensions (ninds, nsites) containing the sequences
+            A numpy array of dimensions (ninds, nsites) containing the seqs
         """
         # generate a random sequence of length nsites
         oseq = np.random.choice(list("ACGT"), size=self.nsites)
@@ -74,15 +35,15 @@ class Seqlib:
         # create a random mask for array - prob of mutation for any site is 0.1
         muts = np.random.binomial(1, 0.1, (self.ninds, self.nsites))
 
-        # mutate sequences
+        # _mutate sequences
         for col in range(self.nsites):
             # pick a mutation
-            newbase = mutate(arr[0, col])
+            newbase = self._mutate(arr[0, col])
 
             # create mask for col from muts
             mask = muts[:, col].astype(bool)
 
-            # mutate randomly selected rows
+            # _mutate randomly selected rows
             arr[:, col][mask] = newbase
 
         # create a random mask for missing bases and modify arr
@@ -91,20 +52,107 @@ class Seqlib:
 
         return arr
 
-    def filter(self, minmaf, maxmissing):
-        "Wrapper to combine filter_missing and filter_maf into one function"
-        return(filter_missing(arr=filter_maf(arr=self.seqs,
-                                             minfreq=minmaf),
-                              maxfreq=maxmissing))
+    def _get_maf(self):
+        """Get an array of minor allele frequencies"""
+        # determine proportion of alleles in a col that do not match row 1
+        freqs = np.sum(self.seqs != self.seqs[0], axis=0) / self.seqs.shape[0]
 
-    def calculate_statistics(self):
-        "Return several statistics for a numpy array of DNA sequences"
+        # create a copy of freqs with all values > 0.5 changed to complements
+        maf = freqs.copy()
+        maf[maf > 0.5] = 1 - maf[maf > 0.5]
+
+        return maf
+
+    def _filter_missing(self, maxfreq):
+        """
+        Remove sites with many missing reads from seqs
+
+        Args:
+            maxfreq: The critical frequency for missing reads.
+                     If the frequency of N at a site exceeds maxfreq, that site
+                     is excluded from the result
+
+        Returns:
+            A boolean filter for seqs indicating cols with freq of N <= maxfreq
+        """
+        # determine the frequency of missing reads at a site
+        freqmissing = np.sum(self.seqs == "N", axis=0) / self.seqs.shape[0]
+
+        # return arr, sliced to only include cols with freqmissing <= maxfreq
+        return freqmissing <= maxfreq
+
+    def _filter_maf(self, minfreq):
+        """
+        Filters seqs to only include sites at which a minimum proportion of
+        alleles differ from the most common allele
+
+        Args:
+            minfreq: the minimum proportion of alleles which must differ from
+                     the most common allele at a site
+
+        Returns:
+            A boolean filter for seqs indicating cols with maf > minfreq
+        """
+
+        return self.maf > minfreq
+
+    # public functions
+    def filter(self, minmaf=0, maxmissing=1):
+        """
+        Public wrapper to combine filter_missing and filter_maf
+
+        Args:
+            minmaf: the minimum minor allele frequency
+            maxmissing: the maximum frequency of missing reads
+
+        Returns:
+            A filtered view of self.seqs, as defined by minmaf and maxmissing
+            Default values return self.seqs in full
+        """
+
+        maf_filter = self._filter_maf(minmaf)
+        miss_filter = self._filter_missing(maxmissing)
+
+        all_filter = maf_filter + miss_filter
+
+        return self.seqs[:, all_filter]
+
+    def filter_seqlib(self, minmaf=0, maxmissing=1):
+        """
+        Version of filter that returns a new Seqlib object
+
+        Args:
+            minmaf: the minimum minor allele frequency
+            maxmissing: the maximum frequency of missing reads
+
+        Returns:
+            A new Seqlib object with a filtered version of seqs as new.seqs
+            Default values return a copy of self in full
+        """
+        # apply filters to get new seqs
+        newseqs = self.filter(minmaf, maxmissing)
+
+        # make a new copy of the Seqlib object
+        newself = copy.deepcopy(self)
+        newself.__init__(newseqs.shape[0], newseqs.shape[1])
+
+        # overwrite the seqs array
+        newself.seqs = newseqs
+
+        # call _get_maf to match maf to array
+        newself._get_maf()
+
+        return newself
+
+    def calculate_stats(self):
+        """Return several statistics for a numpy array of DNA sequences"""
         # mean nucleotide diversity
         nd = np.var(self.seqs == self.seqs[0], axis=0).mean()
         # mean frequency of minor allele at each site
-        mf = np.mean(np.sum(self.seqs != self.seqs[0], axis=0) / self.seqs.shape[0])
+        mf = np.mean(np.sum(self.seqs != self.seqs[0], axis=0)
+                     / self.seqs.shape[0])
         # number of sites with no mutations or missing reads
-        inv = np.any(self.seqs != self.seqs[0], axis=0).sum()
+        inv = np.all(self.seqs == self.seqs[0], axis=0).sum()
         # number of sites with mutations and missing reads
         var = self.seqs.shape[1] - inv
 
